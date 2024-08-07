a1cf99ae9948a58d3db18168e08a592bdec36344
mohamedo7x <mohamedo7x@hotmail.com>
2024-08-07 17:00:40 +0300

    tree methods

--- data_structure/cpp/Tree/main/main.cpp
+++ data_structure/cpp/Tree/main/main.cpp
@@ -3,232 +3,292 @@
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <queue>
 #include <math.h>
 using namespace std;
+
 template <typename T>
-class BinaryTree final {
-    private:// ------------------------------------->>>>>>> PRIVATE <<<<<<-----------------------------------
-    struct Node {
+class BinaryTree final
+{
+private:
+    struct Node
+    {
         T data;
-        Node *left {};
-        Node * right {};
-        Node (T data):data(data){}
+        Node *left{};
+        Node *right{};
+        Node(T data) : data(data) {}
     };
-    Node *root {};
-    void print_in_oreder (Node * cur) {
-        if(!cur)
-            return;
-        print_in_oreder(cur->left);
-        cout << cur->data << " ";
-        print_in_oreder(cur->right);
-    }
-    void print_post_oreder (Node * cur) {
-        if(!cur)
-            return;
-        print_post_oreder(cur->left);
-        print_post_oreder(cur->right);
-        cout << cur->data << " ";
-    }
-    T tree_max(Node *cur){ 
-        if(!cur)
-            return 0;
-        int left_ = tree_max(cur->left);
-        int right_ = tree_max(cur->right);
-        return max(cur->data , max(left_ , right_));
-    }
-    int tree_height(Node *cur) {
-        if(!cur)
-            return 0;
-        int left_ = 1+tree_height(cur->left);
-        int right_ = 1+tree_height(cur->right);
-        return max(left_ , right_);
-    }
-    int tree_diametre (Node *cur) {
-     if(!cur)
-            return 0;
-        int left_ = 1+tree_height(cur->left);
-        int right_ = 1+tree_height(cur->right);
-        return left_+right_;
-    }
-    int count_all_node (Node * cur) {
-            if(!cur)
-                return 0;
-            int left_ = 1+count_all_node(cur->left);
-            int right_ = count_all_node(cur->right);
-            return left_+right_;
-    }
-    bool search (Node * cur , int value) {
-        if(!cur)return false;
-        if(cur->data == value) return true;
-        bool a=  search(cur->left , value);
+    Node *root{};
 
-        bool b= search(cur->right , value);
-        return a || b;
+    void print_in_order(Node *cur)
+    {
+        if (!cur)
+            return;
+        print_in_order(cur->left);
+        cout << cur->data << " ";
+        print_in_order(cur->right);
     }
-    int count_leaf_node (Node * cur) {
-        if(!cur->left& !cur->right ){
-            return 1;
-        }
-        int left = count_leaf_node(cur->left);
-        int right = count_leaf_node(cur->right);
-        return left+right;
-    } 
-    bool is_prefect (int highet, Node * cur){
-        if(highet == -1){
-            highet = this->tree_height(this->root);
-        }
-        if(!cur->left && !cur->right){
+
+    void print_post_order(Node *cur)
+    {
+        if (!cur)
+            return;
+        print_post_order(cur->left);
+        print_post_order(cur->right);
+        cout << cur->data << " ";
+    }
+
+    T tree_max(Node *cur)
+    {
+        if (!cur)
             return 0;
-        }
-        if(!cur->left && cur->right || !cur->right && cur->left){
+        T left_ = tree_max(cur->left);
+        T right_ = tree_max(cur->right);
+        return max(cur->data, max(left_, right_));
+    }
+
+    int tree_height(Node *cur)
+    {
+        if (!cur)
+            return 0;
+        int left_ = 1 + tree_height(cur->left);
+        int right_ = 1 + tree_height(cur->right);
+        return max(left_, right_);
+    }
+
+    int tree_diameter(Node *cur)
+    {
+        if (!cur)
+            return 0;
+        int left_ = tree_height(cur->left);
+        int right_ = tree_height(cur->right);
+        return left_ + right_ + 1;
+    }
+
+    int count_all_nodes(Node *cur)
+    {
+        if (!cur)
+            return 0;
+        int left_ = count_all_nodes(cur->left);
+        int right_ = count_all_nodes(cur->right);
+        return 1 + left_ + right_;
+    }
+
+    bool search(Node *cur, T value)
+    {
+        if (!cur)
             return false;
-        }
-        bool left = is_prefect(highet , cur->left);
-        bool right = is_prefect(highet, cur->left);
-        return left && right;
+        if (cur->data == value)
+            return true;
+        return search(cur->left, value) || search(cur->right, value);
     }
-    void print_in_order_iterative (Node * cur) {
-        stack<Node*> stk;
-        while(true){
-            if(cur !=nullptr){
-            stk.push(cur);
-            cur = cur->left;
-            }else {
-                if(stk.empty()) break;
-                cout <<stk.top()->data<<" ";
-                cur = stk.top()->right;
-                stk.pop();
-            }
-        }
-    }
-    void traverse_left_boundry (Node * cur) {
-        if(!cur) return;
-        cout << cur->data << " ";
-        traverse_left_boundry(cur->left);
 
+    int count_leaf_nodes(Node *cur)
+    {
+        if (!cur)
+            return 0;
+        if (!cur->left && !cur->right)
+            return 1;
+        return count_leaf_nodes(cur->left) + count_leaf_nodes(cur->right);
     }
-    int sol = 0;
-    int b_tree_diamter (Node * cur) {
-        if(!cur) return -1;
-        int left = b_tree_diamter(cur->left);
-        int right = b_tree_diamter(cur->right);
-        sol = max(sol , 2+left+right);
-        return 1 + max(left , right);
+
+    bool is_perfect(Node *cur, int d, int level = 0)
+    {
+        if (!cur)
+            return true;
+        if (!cur->left && !cur->right)
+            return (d == level + 1);
+        if (!cur->left || !cur->right)
+            return false;
+        return is_perfect(cur->left, d, level + 1) && is_perfect(cur->right, d, level + 1);
     }
-    public: // ------------------------------------->>>>>>> PUBLIC <<<<<<-----------------------------------
-    void print_in_order () {
-        this->print_in_oreder(this->root);
-        cout << endl;
-    }
-    void print_post_order () {
-        this->print_post_oreder(this->root);
-        cout << endl;
-    }
-    void add(vector<int> nodes, vector<char> path) {
-    assert(nodes.size() == path.size());
-    Node* temp = root;
-    for (int i = 0; i < (int)path.size(); i++) {
-        if (path[i] == 'L') {
-            if (!temp->left) {
-                temp->left = new Node(nodes[i]);
-            } else {
-                assert(temp->left->data == nodes[i]);
+
+    void print_in_order_iterative(Node *cur)
+    {
+        stack<Node *> stk;
+        while (true)
+        {
+            if (cur != nullptr)
+            {
+                stk.push(cur);
+                cur = cur->left;
             }
-            temp = temp->left;
-        } else if (path[i] == 'R') {
-            if (!temp->right) {
-                temp->right = new Node(nodes[i]);
-            } else {
-                assert(temp->right->data == nodes[i]);
-            }
-            temp = temp->right;
+            else
+            {
+                if (stk.empty())
+                    break;
+                cur = stk.top();
+                stk.pop();
+                cout << cur->data << " ";
+                cur = cur->right;
             }
         }
     }
-    void print_root (){
-        cout << (T)(root->data) << endl;
-    }
-    BinaryTree(T data){
-        root = new Node(data);
-      
-    }
-    void destoryBtree (Node * cur) {
-        if(cur){
-            destoryBtree(cur->left);
-            destoryBtree(cur->right);
+
+    void destroy_tree(Node *cur)
+    {
+        if (cur)
+        {
+            destroy_tree(cur->left);
+            destroy_tree(cur->right);
             delete cur;
         }
     }
-    int tree_max () {
-        return this->tree_max(this->root);
+
+public:
+    BinaryTree(T data)
+    {
+        root = new Node(data);
     }
-    ~BinaryTree(){
-        destoryBtree(this->root);
+
+    ~BinaryTree()
+    {
+        destroy_tree(root);
     }
-    void bulid_fast_tree (){
+
+    void print_in_order()
+    {
+        print_in_order(root);
+        cout << endl;
+    }
+
+    void print_post_order()
+    {
+        print_post_order(root);
+        cout << endl;
+    }
+
+    void add(vector<int> nodes, vector<char> path)
+    {
+        assert(nodes.size() == path.size());
+        Node *temp = root;
+        for (int i = 0; i < (int)path.size(); i++)
+        {
+            if (path[i] == 'L')
+            {
+                if (!temp->left)
+                {
+                    temp->left = new Node(nodes[i]);
+                }
+                else
+                {
+                    assert(temp->left->data == nodes[i]);
+                }
+                temp = temp->left;
+            }
+            else if (path[i] == 'R')
+            {
+                if (!temp->right)
+                {
+                    temp->right = new Node(nodes[i]);
+                }
+                else
+                {
+                    assert(temp->right->data == nodes[i]);
+                }
+                temp = temp->right;
+            }
+        }
+    }
+
+    void build_fast_tree()
+    {
         root->data = 2;
         root->left = new Node(3);
         root->right = new Node(13);
-        Node * temp = root;
-        temp = temp->right;
+        Node *temp = root->right;
         temp->left = new Node(7);
         temp->right = new Node(8);
     }
-    void bulid_fast_tree_v2 (){
-    root->data = 1;
-    root->left = new Node(2);
-    root->right = new Node(3);
-    Node * temp = root;
-    temp = temp->right;
-    temp->left = new Node(4);
-    temp->right = new Node(5);
+
+    void build_fast_tree_v2()
+    {
+        root->data = 1;
+        root->left = new Node(2);
+        root->right = new Node(3);
+        Node *temp = root->right;
+        temp->left = new Node(4);
+        temp->right = new Node(5);
     }
-    int tree_height () {
-        return tree_height(this->root) -1 ;
+
+    int tree_height()
+    {
+        return tree_height(root);
     }
-    int count_all_node () {
-        return this->count_all_node(this->root);
+
+    int count_all_nodes()
+    {
+        return count_all_nodes(root);
     }
-    bool search (int data) {
-        return this->search(this->root ,data );
+
+    bool search(T data)
+    {
+        return search(root, data);
     }
-    int count_leaf_node () {
-        return this->count_leaf_node(this->root);
+
+    int count_leaf_nodes()
+    {
+        return count_leaf_nodes(root);
     }
-     bool is_perfect_formula () {
-        int h = this->tree_height(this->root);
-        int n = this->count_all_node(this->root);
-        return pow(2, h+1)-1 == n;
-    } 
-    bool is_prefect () {
-        return this->is_prefect((int)this->tree_height(this->root),this->root);
+
+    bool is_perfect()
+    {
+        int h = tree_height(root);
+        return is_perfect(root, h);
     }
-    void print_in_order_iterative () {
-        print_in_order_iterative(this->root);
+
+    void print_in_order_iterative()
+    {
+        print_in_order_iterative(root);
     }
-    void traverse_left_boundry(){
-        this->traverse_left_boundry(this->root);
+
+    int tree_diameter()
+    {
+        return tree_diameter(root);
     }
-    int tree_diametre () {
-        return this->tree_diametre(this->root);
+
+    void level_order_traversal()
+    {
+        Node * cur = this->root;
+        queue<Node*> q;
+        q.push(cur);
+        while (!q.empty())
+        {
+            if (q.front()->left)
+                q.push(q.front()->left);
+            if (q.front()->right)
+                q.push(q.front()->right);
+            cout << q.front()->data << " ";
+            q.pop();
+        }
     }
-    int b_tree_diamter () {
-        this->b_tree_diamter(this->root);
-        return sol;
+    void bfs () {
+        queue<Node*> q;
+        int level = 0 ;
+        q.push(this->root);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                Node *cur = q.front();
+                q.pop();
+                cout << cur->data << ' ';
+                if(cur->left){
+                    q.push(cur->left);
+                }
+                if(cur->right)
+                    q.push(cur->right);
+            }
+            level++;
+            cout << endl;
+        }
     }
 };
 
-int main(){
-    BinaryTree tree(1); // Root
-    // tree.add({2,4,7},{'L','L','L'});
-    // tree.add({2,4,8},{'L','L','R'});
-    // tree.add({2,5,9},{'L','R','R'});
-    // tree.add({3,6,10},{'R','R','L'});
-    tree.bulid_fast_tree_v2();
-    // tree.print_in_order();
-    // tree.print_in_order_iterative();
-    // cout << "print " << tree.is_prefect();
-    // cout << tree.tree_diametre() -2 ;
-    cout <<"Diamter " <<tree.b_tree_diamter();
+int main()
+{
+    BinaryTree<int> tree(1); // Root
+    tree.build_fast_tree_v2();
+    tree.level_order_traversal();
     cout << "\n\n!! NO RTE !!\n\n";
+    return 0;
 }
--- data_structure/cpp/Tree/secondary/main.cpp
+++ data_structure/cpp/Tree/secondary/main.cpp
@@ -1,84 +1,96 @@
 #include <iostream>
 #include <vector>
-#include <assert.h>
-#include <algorithm>
-#include <bits/stdc++.h>
+#include <stack>
+#include <cassert>
+#include <cctype>
 
 using namespace std;
+
 template<typename T>
 class BinaryTree {
-
-    private:
+private:
     T data;
-    BinaryTree * left {};
-    BinaryTree * right {};
-    public:
+    BinaryTree *left{};
+    BinaryTree *right{};
+
+public:
     void add(vector<T> values, vector<char> direction) {
-		assert(values.size() == direction.size());
-		BinaryTree* current = this;
-		// iterate on the path, create all necessary nodes
-		for (int i = 0; i < (int) values.size(); ++i) {
-			if (direction[i] == 'L') {
-				if (!current->left)
-					current->left = new BinaryTree(values[i]);
-				else
-					assert(current->left->data == values[i]);
-				current = current->left;
-			} else {
-				if (!current->right)
-					current->right = new BinaryTree(values[i]);
-				else
-					assert(current->right->data == values[i]);
-				current = current->right;
-			}
-		}
-	}
+        assert(values.size() == direction.size());
+        BinaryTree* current = this;
+        for (int i = 0; i < (int)values.size(); ++i) {
+            if (direction[i] == 'L') {
+                if (!current->left)
+                    current->left = new BinaryTree(values[i]);
+                else
+                    assert(current->left->data == values[i]);
+                current = current->left;
+            } else {
+                if (!current->right)
+                    current->right = new BinaryTree(values[i]);
+                else
+                    assert(current->right->data == values[i]);
+                current = current->right;
+            }
+        }
+    }
+
     void print_inorder() {
-		if (left)
-			left->print_inorder();
+        if (left)
+            left->print_inorder();
+        cout << data << " ";
+        if (right)
+            right->print_inorder();
+    }
+
+	void print_post_order() {
+		if (left){
+            left->print_inorder();
+		}
+        if (right){
+            right->print_inorder();
+		}
 		cout << data << " ";
-		if (right)
-			right->print_inorder();
-	}
+    }
+
     int tree_max() {
-		int res = data;
-		if (left)
-			res = max(res, left->tree_max());
-		if (right)
-			res = max(res, right->tree_max());
-		return res;
-	}
-    BinaryTree(int data) :
-			data(data) {
-	}
-	BinaryTree(string str) {
-		int total_nodes = -1;
-		this->data = str[str.size()-1]; // root
-		vector<char> nodes;
-		for(int i = 1; i<str.size() ; i++){
-			nodes.push_back(str[i]);
-			total_nodes+=1;
-		}
-		reverse(nodes.begin() , nodes.end());
-		bool flag = false;
-		int counter = 0;
-		while(total_nodes--){
-			if(!flag){
-				vector<char> a(1);
-				a[counter] = nodes[counter];
-				add(a , {'L'});
-				counter++;
-			}else {
-				vector<char> a(1);
-				a[counter] = nodes[counter];
-				add(a , {'R'});
-				counter++;
-				
-			}
-		}
-	}
+        int res = data;
+        if (left)
+            res = max(res, left->tree_max());
+        if (right)
+            res = max(res, right->tree_max());
+        return res;
+    }
+
+    BinaryTree(T data) : data(data) {}
+
+    BinaryTree(string str) {
+        stack<BinaryTree*> stk;
+        for (char ch : str) {
+            BinaryTree* cur = new BinaryTree(ch);
+            if (!isdigit(ch)) {
+                cur->right = stk.top();
+                stk.pop();
+                cur->left = stk.top();
+                stk.pop();
+            }
+            stk.push(cur);
+        }
+        BinaryTree* root = stk.top();
+        stk.pop();
+        this->left = root->left;
+        this->data = root->data;
+        this->right = root->right;
+    }
+
+    ~BinaryTree() {
+        delete left;
+        delete right;
+    }
 };
-int main () {
-    BinaryTree<string> tree("23+4*");
-	cout << "\n\n!! NO RTE !!\n\n";
+
+int main() {
+    BinaryTree<char> tree("23+4*");
+    tree.print_post_order();
+    cout << "\n\n!! NO RTE !!\n\n";
+    return 0;
 }
